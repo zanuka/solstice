f0b2a85863a45fe2556f2868b24d7c65862c3b2d
zanuka <git@zanuka.com>
2016-01-16 02:06:37 -0800

    cleanup

--- test/index.js
+++
@@ -1,9 +0,0 @@
-// This file cannot be written with ECMAScript 2015 because it has to load
-// the Babel require hook to enable ECMAScript 2015 features!
-require("babel-register")({
-  optional: "runtime"
-});
-
-// The tests, however, can and should be written with ECMAScript 2015.
-require("./index.test.js");
-
